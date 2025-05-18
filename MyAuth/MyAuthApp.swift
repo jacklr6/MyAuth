983ae24c41d157c5ef8c9ca7df0ab108ffe17c81
jacklr6 <captapplesauce6@gmail.com>
2025-05-18 12:21:28 -0400

    Initial Commit

---
+++ MyAuth/MyAuthApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAuthApp.swift
+//  MyAuth
+//
+//  Created by Jack Rogers on 5/18/25.
+//
+
+import SwiftUI
+
+@main
+struct MyAuthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
